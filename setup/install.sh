7139cc904b60e22cd6833aa3d30bcd23e902bfc2
Denis Arh <denis.arh@gmail.com>
2020-04-01 14:44:19 +0200

    Simplify install instructions

--- setup/install.sh
+++ setup/install.sh
@@ -1,58 +1,28 @@
 #!/bin/bash
-# This is the entry point for configuring the system.
-#####################################################
 
-#install basic tools
-sudo DEBIAN_FRONTEND=noninteractive apt-get update
-sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install git
-echo "127.0.0.1    db" | sudo tee -a /etc/hosts
+set -eu
 
-# Setup the base OS
-sudo DEBIAN_FRONTEND=noninteractive apt-get update -qq \
- && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends build-essential  \
-    apt-transport-https curl ca-certificates gnupg2 apt-utils nodejs
+export DEBIAN_FRONTEND=noninteractive
 
-# Install percona 8
-wget https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
-sudo DEBIAN_FRONTEND=noninteractive dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
-sudo percona-release setup ps80
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y percona-server-server
-sudo mysql < /home/vagrant/workspace/src/percona-db.sql
+apt-get update -qq
+apt-get -y -q \
+  -o Dpkg::Options::="--force-confdef" \
+  -o Dpkg::Options::="--force-confnew" \
+  install \
+    --no-install-recommends \
+      git \
+      build-essential  \
+      apt-transport-https \
+      curl \
+      ca-certificates \
+      gnupg2 \
+      apt-utils \
+      percona-server-server \
+      nodejs
 
-# Install node from nodesource
-# uncomment the next 2 lines for fix
-curl -sL https://deb.nodesource.com/setup_10.x | sudo DEBIAN_FRONTEND=noninteractive bash - \
-  && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y nodejs
+wget --quiet https://repo.percona.com/apt/percona-release_latest.$(lsb_release -sc)_all.deb
+dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
+percona-release setup ps80
+mysql < /vagrant/setup/percona-db.sql
 
-# Install yarn
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo DEBIAN_FRONTEND=noninteractive apt-key add - \
- && echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list \
- && sudo DEBIAN_FRONTEND=noninteractive apt-get update -qq \
- && sudo DEBIAN_FRONTEND=noninteractive apt-get install -y yarn
-
-#get golang
-curl -O https://dl.google.com/go/go1.12.17.linux-amd64.tar.gz
-
-#unzip the archive 
-tar -xvf go1.12.17.linux-amd64.tar.gz
-
-#move the go lib to local folder
-mv go /usr/local
-
-#delete the source file
-rm  go1.12.17.linux-amd64.tar.gz
-
-#only full path will work
-touch /home/vagrant/.bash_profile
-
-echo "export PATH=$PATH:/usr/local/go/bin" >> /home/vagrant/.bash_profile
-
-echo `export GOPATH=/home/vagrant/workspace:$PATH` >> /home/vagrant/.bash_profile
-
-export GOPATH=/home/vagrant/workspace
-
-mkdir -p "$GOPATH/bin" 
-
-## INSTALL Vue for corteza-webapp-one
-yarn global add @vue/cli
-yarn install
+#
